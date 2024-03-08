0f42d80bdaa2f52a06d47643ccdcd965301ef96c
SkyRate <kopeyko.96@mail.ru>
2024-03-08 21:25:48 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://SkyRate.github.io/travel",
+  base: "https://SkyRate.github.io/Travel",
 });
